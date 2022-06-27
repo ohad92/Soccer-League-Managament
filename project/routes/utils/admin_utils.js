01ad127e7604d525e768c69ee08128d1222018ec
ohad92 <73739848+ohad92@users.noreply.github.com>
2022-06-27 14:43:13 +0300

    Delete project directory

--- project/routes/utils/admin_utils.js
+++
@@ -1,8 +0,0 @@
-const axios = require("axios");
-const DBUtills=require("./DButils");
-
-async function getadmin(){
-    return (await DBUtills.execQuery(`SELECT username FROM Users`))[0];
-}
-
-exports.getadmin = getadmin;
